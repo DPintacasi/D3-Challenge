bcc4831ca84329684e3d2b2a3fd766e439523b24
DPintacasi <73974414+DPintacasi@users.noreply.github.com>
2021-04-19 19:13:09 -0400

    animated axes, renamed labels

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ var margin = {
   top: svgHeight*0.1,
   right: svgWidth*0.10,
   bottom: svgHeight*0.25,
-  left: svgWidth*0.15
+  left: svgWidth*0.20
 };
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
@@ -27,7 +27,7 @@ var chartGroup = svg.append("g")
 .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // x axis label/options
-const xoptions = ["income", "age", "poverty"]
+const xoptions = ["Median Income", "Median Age", "Poverty Rate"]
 chartGroup
 .selectAll("div")
 .data(xoptions)
@@ -41,7 +41,7 @@ chartGroup
 .text(d => d);
 
 // y axis label
-const yoptions = ["healthcare", "obesity", "smokes"]
+const yoptions = ["Lacks Healthcare (%)", "Obesity Rate", "Smokes (%)"]
 chartGroup
 .selectAll("div")
 .data(yoptions)
@@ -55,15 +55,14 @@ chartGroup
 .classed("yOptions", "true")
 .text(d => d);
 
-// default labels
+// default selected labels
 d3.selectAll(".aText")
 .filter(function(){ 
-  return d3.select(this).text() === "healthcare" || d3.select(this).text() === "income"
+  return d3.select(this).text() === "Lacks Healthcare (%)" || d3.select(this).text() === "Median Income"
 })
 .classed("inactive", false)
 .classed("active", true);
 
-// Event Listener and Handler for Axis Chooser
 
 d3.csv("assets/data/data.csv").then(function(data){
     
@@ -79,70 +78,72 @@ d3.csv("assets/data/data.csv").then(function(data){
   });
 
   function init_chart(){
-  //scalar functions
-  var xLinearScale = d3.scaleLinear()
-  .domain(d3.extent(data, d => d.income))
-  .range([0, width]);
+    //scalar functions
+    var xLinearScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.income))
+    .range([0, width]);
 
-  var yLinearScale = d3.scaleLinear()
-  .domain([0,d3.max(data, d => d.healthcare)])
-  .range([height, 0]);
+    var yLinearScale = d3.scaleLinear()
+    .domain([0,d3.max(data, d => d.healthcare)])
+    .range([height, 0]);
 
-  // axes
-  var xAxis = d3.axisBottom(xLinearScale).ticks(6);
-  var yAxis = d3.axisLeft(yLinearScale).ticks(6);
+    // axes
+    var xAxis = d3.axisBottom(xLinearScale).ticks(6);
+    var yAxis = d3.axisLeft(yLinearScale).ticks(6);
 
-  chartGroup.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(xAxis);
+    chartGroup.append("g")
+    .attr("id", "xAxis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
 
-  chartGroup.append("g")
-  .call(yAxis);
+    chartGroup.append("g")
+    .attr("id", "yAxis")
+    .call(yAxis);
 
-  //data points
-  var radius = 10;
-  var circlesGroup = chartGroup.selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.income))
-  .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", radius )
-  .classed("stateCircle", "true");
+    //data points
+    var radius = 10;
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.income))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", radius )
+    .classed("stateCircle", "true");
 
-  // state labels
-  chartGroup
-  .selectAll("div")
-  .data(data)
-  .enter()
-  .append("text")
-  .attr("x", d => xLinearScale(d.income))
-  .attr("y", d => yLinearScale(d.healthcare)+3)
-  .text(d => d.abbr)
-  .classed("stateText", "true")
-  .style("font-size", `${radius}px`);
+    // state labels
+    chartGroup
+    .selectAll("div")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("x", d => xLinearScale(d.income))
+    .attr("y", d => yLinearScale(d.healthcare)+3)
+    .text(d => d.abbr)
+    .classed("stateText", "true")
+    .style("font-size", `${radius}px`);
 
-  // tool tip
-  var toolTip = d3.tip()
-  .attr("class", "d3-tip")
-  .offset([10, 30])
-  .html(function(d) {
-    return (`<strong>${d.state}</strong><br> 
-    Median Income: ${d.income} <br> 
-    Access to healthcare: ${d.healthcare} <br> `);
-  });
+    // tool tip
+    var toolTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([10, 30])
+    .html(function(d) {
+      return (`<strong>${d.state}</strong><br> 
+      Median Income: ${d.income} <br> 
+      Access to healthcare: ${d.healthcare} <br> `);
+    });
 
-  // add tooltip in the chart
-  chartGroup.call(toolTip);
+    // add tooltip in the chart
+    chartGroup.call(toolTip);
 
-  // event listeners to display and hide the tooltip
-  circlesGroup.on("mouseover", function(d) {
-    toolTip.show(d, this);
-  });
+    // event listeners to display and hide the tooltip
+    circlesGroup.on("mouseover", function(d) {
+      toolTip.show(d, this);
+    });
 
-  circlesGroup.on("mouseout", function(d) {
-    toolTip.hide(d);
-  });
+    circlesGroup.on("mouseout", function(d) {
+      toolTip.hide(d);
+    });
 
   };
 
@@ -150,7 +151,7 @@ d3.csv("assets/data/data.csv").then(function(data){
 
   // click event for choosing an axes
   d3.selectAll(".aText").on("click", function(){
-    // console.log(this);
+
     var selection = d3.select(this);
     if (selection.classed("xOptions")){
       d3.selectAll(".xOptions").classed("inactive", true);
@@ -168,8 +169,16 @@ d3.csv("assets/data/data.csv").then(function(data){
     console.log(`Variable selection: ${selection.text()}`);
     console.log("It is on the X axis");
 
-    array = data.map((d)=> d[selection.text()])
-    console.log(array)
+    const xLabels = {
+      "Median Income" : "income",
+      "Median Age" : "age",
+      "Poverty Rate" : "poverty"
+    };
+
+    var selectionValue = xLabels[selection.text()];
+
+    array = data.map((d)=> d[selectionValue]);
+    console.log(array);
 
     var scale = d3.scaleLinear()
     .domain(d3.extent(array))
@@ -186,7 +195,12 @@ d3.csv("assets/data/data.csv").then(function(data){
     .duration(1000)
     .attr("x", (d, i) => scale(array[i]))
 
-    
+    var axis = d3.axisBottom(scale).ticks(6);
+
+    chartGroup.select("#xAxis")
+    .transition()
+    .duration(1000)
+    .call(axis);
 
   };
 
@@ -194,8 +208,17 @@ d3.csv("assets/data/data.csv").then(function(data){
     console.log(`Variable selection: ${selection.text()}`);
     console.log("It is on the Y axis");
 
-    array = data.map((d)=> d[selection.text()])
-    console.log(array)
+    // const yoptions = ["Lacks Healthcare (%)", "Obesity Rate", "Smokes (%)"]
+
+    const yLabels = {
+      "Lacks Healthcare (%)" : "healthcare",
+      "Obesity Rate" : "obesity",
+      "Smokes (%)" : "smokes"
+    };
+    
+    var selectionValue = yLabels[selection.text()];
+    array = data.map((d)=> d[selectionValue]);
+    console.log(array);
 
     var scale = d3.scaleLinear()
     .domain(d3.extent(array))
@@ -205,12 +228,19 @@ d3.csv("assets/data/data.csv").then(function(data){
     chartGroup.selectAll("circle")
     .transition()
     .duration(1000)
-    .attr("cy", (d, i) => scale(array[i]))
+    .attr("cy", (d, i) => scale(array[i]));
 
     chartGroup.selectAll(".stateText")
     .transition()
     .duration(1000)
-    .attr("y", (d, i) => scale(array[i]))
+    .attr("y", (d, i) => scale(array[i])+3);
+
+    var axis = d3.axisLeft(scale).ticks(6);
+
+    chartGroup.select("#yAxis")
+    .transition()
+    .duration(1000)
+    .call(axis);
 
   };
 
